db474acd507b2ebd9cc690fa2c587e8860f21c34
Fatih Ay <mfatihay@gmail.com>
2022-03-13 15:09:36 +0300

    if bloğu ile ilgili bir nokta

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-const firstName = 'Fatih';
-const job = "teacher";
-const birthyear = 1979;
-const year = 2022;
+// const firstName = 'Fatih';
+// const job = "teacher";
+// const birthyear = 1979;
+// const year = 2022;
 
-// TEMPLATE LITERAL
+// // TEMPLATE LITERAL
 
-const fatih = `I'm ${firstName}, a ${year - birthyear} year old ${job}`;
-// backtick için altgr + virgül
-// ES6 sonrası
+// const fatih = `I'm ${firstName}, a ${year - birthyear} year old ${job}`;
+// // backtick için altgr + virgül
+// // ES6 sonrası
 
-console.log(fatih);
-console.log(`Just a regular string..`);
+// console.log(fatih);
+// console.log(`Just a regular string..`);
 
-console.log('String \n\multiple \n\lines'); //es6 öncesi
+// console.log('String \n\multiple \n\lines'); //es6 öncesi
 
-console.log(`String
-multiple
-lines
-after ES6`);
-// after ES6
+// console.log(`String
+// multiple
+// lines
+// after ES6`);
+// // after ES6
 
+const birthYear = 1998;
+
+let century;
+
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century);
+// değişkeni if bloğu içinde oluştursaydık, if dışında kullanamazdık
