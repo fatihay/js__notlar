445739946cc807d8fa0e3a1ee6496758cb41f992
Fatih Ay <mfatihay@gmail.com>
2022-03-13 21:20:47 +0300

    conversion and coercion

--- script.js
+++ script.js
@@ -20,15 +20,31 @@
 // after ES6`);
 // // after ES6
 
-const birthYear = 1998;
+// const birthYear = 1998;
 
-let century;
+// let century;
 
-if (birthYear <= 2000) {
-    century = 20;
-} else {
-    century = 21;
-}
+// if (birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
 
-console.log(century);
-// değişkeni if bloğu içinde oluştursaydık, if dışında kullanamazdık
+// console.log(century);
+// // değişkeni if bloğu içinde oluştursaydık, if dışında kullanamazdık
+
+
+// TYPE CONVERSION
+// const inputYear = '1991';
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) + 18);
+// console.log(String(23));
+
+// // TYPE COERCION
+// console.log('I am ' + 23 + 'years old');
+// console.log('23' - '10' - 4);
+// // javascript otomatik olarak sayıya çevirdi
+
+// console.log('23' + '10' + 4);
+// // Bunda otomatik olarak stringe çevirdi
+
