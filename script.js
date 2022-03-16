9147f06cf97a71c612b281ac38a451a769e95aba
fatihay <61990016+fatihay@users.noreply.github.com>
2022-03-16 16:46:30 +0300

    js fundamentals 16 mart

--- script.js
+++ script.js
@@ -71,3 +71,35 @@
 // // string dönüyor
 
 // if (favourite !== 23) console.log('Why not 23?');
+
+
+//  ! -- not operator
+// &&  -- and operator
+// ||  -- or operator
+
+
+// const day = 'monday';
+
+// switch (day) {
+//   case 'monday':  // day === 'monday'
+//     console.log('Plan course structure');
+//     console.log('Go to coding meetup');
+//     break;
+
+//   case 'tuesday':  // day === 'tuesday'
+//     console.log('Prepare theory videos');
+//     break;
+
+//   case 'wednesday':
+//   case 'thursday':
+//     console.log('Write code examples');
+//     break;
+
+//   default:
+//     console.log('Not a valid')
+// }
+
+// const age = 23;
+// age >= 18 ? console.log('I like to drink tea') : console.log("I don't like to drink tea");
+// // conditional (ternary) operator
+
