21abded413d66c7b5cb31de2179823d68afdd7cf
fatihay <61990016+fatihay@users.noreply.github.com>
2022-03-21 16:28:50 +0300

    javascript ders 21 mart

--- script.js
+++ script.js
@@ -1,124 +1,120 @@
-// const firstName = 'Fatih';
-// const job = "teacher";
-// const birthyear = 1979;
-// const year = 2022;
+'use strict';
 
-// // TEMPLATE LITERAL
+///////////////////////////////////////
+// Activating Strict Mode
+// let hasDriversLicense = false;
+// const passTest = true;
 
-// const fatih = `I'm ${firstName}, a ${year - birthyear} year old ${job}`;
-// // backtick için altgr + virgül
-// // ES6 sonrası
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive :D');
 
-// console.log(fatih);
-// console.log(`Just a regular string..`);
+// // const interface = 'Audio';
+// // const private = 534;
 
-// console.log('String \n\multiple \n\lines'); //es6 öncesi
-
-// console.log(`String
-// multiple
-// lines
-// after ES6`);
-// // after ES6
-
-// const birthYear = 1998;
-
-// let century;
-
-// if (birthYear <= 2000) {
-//     century = 20;
-// } else {
-//     century = 21;
+// ///////////////////////////////////////
+// // Functions
+// function logger() {
+//   console.log('My name is Jonas');
 // }
 
-// console.log(century);
-// // değişkeni if bloğu içinde oluştursaydık, if dışında kullanamazdık
+// // calling / running / invoking function
+// logger();
+// logger();
+// logger();
 
-
-// TYPE CONVERSION
-// const inputYear = '1991';
-// console.log(Number(inputYear));
-// console.log(Number(inputYear) + 18);
-// console.log(String(23));
-
-// // TYPE COERCION
-// console.log('I am ' + 23 + 'years old');
-// console.log('23' - '10' - 4);
-// // javascript otomatik olarak sayıya çevirdi
-
-// console.log('23' + '10' + 4);
-// // Bunda otomatik olarak stringe çevirdi
-
-// 5 falsy values: 0, '', undefined, null, NaN
-// of course false
-
-// console.log(Boolean(0));
-// console.log(Boolean(undefined));
-// console.log(Boolean('Jonas'));
-// // true
-// console.log(Boolean({}));
-// // true
-
-// const age = 18;
-// if (age === 18) console.log('You just became an adult :)');
-// // if bloğunda tek satırlık kod olunca curly brace'e gerek yok
-// // çift eşit olursa "==" type coercion oluyor
-// // burada '18' ile 18 false sonucu dönüyor
-
-// const favourite = Number(prompt("What's your favourite number?"));
-
-// console.log(favourite);
-// console.log(typeof favourite);
-// // string dönüyor
-
-// if (favourite !== 23) console.log('Why not 23?');
-
-
-//  ! -- not operator
-// &&  -- and operator
-// ||  -- or operator
-
-
-// const day = 'monday';
-
-// switch (day) {
-//   case 'monday':  // day === 'monday'
-//     console.log('Plan course structure');
-//     console.log('Go to coding meetup');
-//     break;
-
-//   case 'tuesday':  // day === 'tuesday'
-//     console.log('Prepare theory videos');
-//     break;
-
-//   case 'wednesday':
-//   case 'thursday':
-//     console.log('Write code examples');
-//     break;
-
-//   default:
-//     console.log('Not a valid')
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
 // }
 
-// const age = 23;
-// age >= 18 ? console.log('I like to drink tea ❤') : console.log("I don't like to drink tea");
-// emoji için windows key + .
-// // conditional (ternary) operator
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
 
-// // const drink = age >= 18 ? 'tea ❤' : 'coffee ☕';
-// // console.log(drink);
-// bu yöntem daha çok kullanılıyor
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
 
-// console.log(`I like to drink ${age >= 18 ? 'tea ❤' : 'coffee ☕'}`);
+// const num = Number('23');
+
+// FUCTION DECLERATION
+// console.log(calcAge1(1979));
+// Function decleration'lar fonksiyon tanımlamadan önce kullanırsak yine çalışıyor
 
 
-// CODING CHALLENGE 4
+// function calcAge1(birthYear) {
+//   // parantez içindeki birthyear parameter
+//   return 2037 - birthYear;
+// }
 
-const bill = 430;
+// console.log(calcAge1(1979));
+// // 1979 is argument
 
-console.log(`Your bill is ${bill}, your tip is ${(bill >= 50 && bill <= 300) ? (bill * 0.15) : (bill * 0.2)} and your total is ${(bill >= 50 && bill <= 300) ? (bill * 1.15) : (bill * 1.2)}`);
 
-// Your bill is 275, your tip is 41.25 and your total is 316.25
+// FUNCTION EXPRESSION
+// console.log(calcAge2(1991));
+//  Function expression'lar tanımlanmadan önce kullanılırsa çalışmıyorlar
+// cannot access calsAge2 before initialization -- uyarı
 
-// Your bill is 40, your tip is 8 and your total is 48
 
-// Your bill is 430, your tip is 86 and your total is 516
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// }
+// // anonymous function - isimsiz fonksiyon
+
+// console.log(calcAge2(1991));
+
+
+// ARROW FUNCTION
+
+// const calcAge3 = birthYear => 2037 - birthYear;
+
+// console.log(calcAge3(1979));
+
+// const yearsUntilRetirement = (birthYear, firstNAme) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   // return retirement;
+//   return `${firstNAme} retires in ${retirement} years`;
+// }
+
+// console.log(yearsUntilRetirement(1979, 'Fatih'));
+
+
+///////////////////////////////////////
+// Introduction to Arrays
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+// friends = ['Bob', 'Alice']
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+// Exercise
+const calcAge = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
